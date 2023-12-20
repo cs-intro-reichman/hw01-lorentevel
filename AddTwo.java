4b80843d44766267b77a982e8b47adf790d87d8d
loren tevel <lorentevel@h-MacBook-Pro-sl-loren.local>
2023-12-20 16:16:37 +0200

    hw1

--- AddTwo.java
+++ AddTwo.java
@@ -2,7 +2,10 @@
  * Adds two given integers and prints the result in a fancy way.
  */
 public class AddTwo {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+	public static void main(String[] args) {	
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int result = a + b;
+		System.out.println(a+" + "+b+ " = "+result);
+ 	}
 }
--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,16 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+		int x = Integer.parseInt(args[0]);
+		int y = Integer.parseInt(args[1]);
+		int a = (int) ((Math.random()*(x-y))+y);
+		int b = (int) ((Math.random()*(x-y))+y);
+		int c = (int) ((Math.random()*(x-y))+y);
+		int d = Math.min(Math.min(a, b), c);
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println("The minimal generated number was "+d);
 	}
 }
