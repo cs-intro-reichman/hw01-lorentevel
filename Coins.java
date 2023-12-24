d0996d97c594313b9bfce794c5f946bb7f3bbe1c
loren tevel <lorentevel@h-MacBook-Pro-sl-loren.local>
2023-12-24 15:17:02 +0200

    try 4

--- Coins.java
+++ Coins.java
@@ -4,9 +4,9 @@
  */
 public class Coins {
 	public static void main(String[] args) {
-	int a = Integer.parseInt(args[0]);
-	int b = a / 25;
-	int c = a % 25;
+		int a = Integer.parseInt(args[0]);
+		int b = a / 25;
+		int c = a % 25;
 		System.out.println(" Use "+b+" quarters "+"and "+c+" cents ");
 	}
 }
--- Triangle.java
+++ Triangle.java
@@ -4,7 +4,7 @@
  * Write a program that tests if three given integers form a triangle.
  */ 
 public class Triangle {
-		public static void main(String[] args) {	
+	public static void main(String[] args) {	
 		int a = Integer.parseInt(args[0]);
 		int b = Integer.parseInt(args[1]);
 		int c = Integer.parseInt(args[2]);
